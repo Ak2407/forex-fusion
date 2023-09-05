a957104a0156fa3833aece51ececa39c146b64eb
Ak2407 <akshu.24gupta@gmail.com>
2023-09-05 18:21:17 +0530

    added github link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className='w-full bg-paragraph mt-10 p-10 flex justify-evenly items-center'>
         <h1 className='font-extrabold text-2xl'>Made by : <Link href={"https://akshit-resume.vercel.app"} target='_blank' className='text-primary underline'>Akshit</Link></h1>
-        <Link href={"https://github.com/Ak2407"} target='_blank' className='text-lg bg-tertiary p-4 rounded-full text-paragraph'>GitHub</Link>
+        <Link href={"https://github.com/Ak2407/forex-fusion"} target='_blank' className='text-lg bg-tertiary p-4 rounded-full text-paragraph'>GitHub</Link>
     </footer>
   )
 }
